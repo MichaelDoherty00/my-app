ada54ddac7be5efa8863b1dc02722c1bc2aba01d
Michael Doherty <mpdoherty00@gmail.com>
2023-12-05 00:35:50 +0000

    adding a title and subtitle

---
+++ src/Title.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Title.css';
+
+function Title() {
+  return (
+    <div className="Title">
+        <div className="Title-Title">
+            <h1>Website</h1>
+        </div>
+        <div className="Title-Subtitle">
+            San Franciscos Privately Owned Public Spaces
+        </div>
+    </div>
+    
+  )
+}
+
+export default Title
